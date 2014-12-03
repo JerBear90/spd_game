98462a5674e5fbdeb0fcf77cf0d253ae361a32e2
Jaramee Flemming <jerameeflemming@gmail.com>
2014-12-02 19:33:39 -0800

    changing css

--- agxgame.js
+++ agxgame.js
@@ -228,7 +228,7 @@ function shuffle(array) {
  */
 var wordPool = [
     {
-        "words"  : [ "<img src=\"http://placehold.it/100x100/eaeaea\">" ,"<img src=\"http://placehold.it/100x100/eaeaea\">" ],
+        "words"  : [ "<img src=\"http://placehold.it/100x100/eaeaea\">", "<img src='http://placehold.it/100x100/eaeaea'>" ],
         "decoys" : [ "lead","lamp","seed","eels","lean","cels","lyse","sloe","tels","self" ]
     },
 
